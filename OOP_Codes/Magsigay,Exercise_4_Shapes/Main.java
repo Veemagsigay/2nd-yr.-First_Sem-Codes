52587d74024155b8bcd4bb81ec9656fa77093649
Veemagsigay <mvpmagsigay@addu.edu.ph>
2023-11-21 13:40:48 +0800

    yhfhf

--- OOP_Codes/Magsigay,Exercise_4_Shapes/Main.java
+++ OOP_Codes/Magsigay,Exercise_4_Shapes/Main.java
@@ -13,7 +13,7 @@ public class Main {
         System.out.println("---------------------------------\n");
         s = new Triangle("Fuschia", 25, 30);
         getArea(s);
-
+//gfyfyfyfyfyf
         
     }
 
